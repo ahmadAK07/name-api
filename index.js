10618f0b6bc8fbb0f651ffc55736cb9387880e97
ahmadAK07 <muhammadahmadamin512@gmail.com>
2024-04-20 17:56:23 +0500

    fourth

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", (req, res)=>{
     res.json({name: "Ahmad"});
 })
-app.get("/:name", (req, res)=>{
+app.get("/input/:name", (req, res)=>{
     res.json({name: req.params.name});
 })
 app.listen(9000, ()=>{
