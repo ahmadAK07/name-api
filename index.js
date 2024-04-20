de08023f8e7b988da67c066992a725c5256579d4
ahmadAK07 <muhammadahmadamin512@gmail.com>
2024-04-20 17:34:43 +0500

    third

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get("/", (req, res)=>{
     res.json({name: "Ahmad"});
 })
 
-app.listen(3000, ()=>{
-    console.log("server is running on 3000")
+app.listen(9000, ()=>{
+    console.log("server is running on 9000")
 })
