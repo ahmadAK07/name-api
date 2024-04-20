8c0ae55483a4dc33858ef9b0b17f213d5fdaf5c5
ahmadAK07 <muhammadahmadamin512@gmail.com>
2024-04-20 17:54:20 +0500

    sixth

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 app.get("/", (req, res)=>{
     res.json({name: "Ahmad"});
 })
-
+app.get("/:name", (req, res)=>{
+    res.json({name: req.params.name});
+})
 app.listen(9000, ()=>{
     console.log("server is running on 9000")
 })
